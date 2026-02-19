cb7cd29ab7fc72db78a6f918d58a72af7e194659
Anuska2103 <anuskabasak2110@gmail.com>
2026-02-19 21:58:04 +0530

    changed embbeding model

--- src/tools/database.py
+++ src/tools/database.py
@@ -1,7 +1,7 @@
 import os
 from pinecone import Pinecone, ServerlessSpec
 from langchain_pinecone import PineconeVectorStore
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,20 +10,19 @@ load_dotenv()
 pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 index_name = os.getenv("PINECONE_INDEX_NAME")
 
-# Use HuggingFace Embeddings (lightweight, free, runs locally)
-# all-MiniLM-L6-v2: 384 dimensions, fast and efficient
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2",
-    model_kwargs={'device': 'cpu'},
-    encode_kwargs={'normalize_embeddings': True}
+
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="models/gemini-embedding-001",
+    google_api_key=os.getenv("GEMINI_API_KEY"),
+    output_dimensionality=768
 )
 
 def init_pinecone_index():
-    """Creates the index if it doesn't exist."""
+    
     if index_name not in pc.list_indexes().names():
         pc.create_index(
             name=index_name,
-            dimension=384, # Dimension for all-MiniLM-L6-v2
+            dimension=768,  
             metric="cosine",
             spec=ServerlessSpec(cloud="aws", region="us-east-1")
         )
